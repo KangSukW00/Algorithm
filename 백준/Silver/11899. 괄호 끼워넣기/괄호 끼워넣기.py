91b15f23e1f0fa57f2a1c0baee35df222b854d66
우강석 <oqz@naver.com>
2025-07-11 22:10:37 +0900

    [Silver III] Title: 괄호 끼워넣기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11899. 괄호 끼워넣기/괄호 끼워넣기.py
@@ -0,0 +1,15 @@
+import sys
+
+s = sys.stdin.readline().rstrip()
+stack = []
+ret = 0
+for c in s:
+	if c == '(': # 여는 괄호가 나오면
+		stack.append(c) # stack에 넣어 둠
+	else: # 닫는 괄호가 나오면
+		if stack: # 여는 괄호가 stack에 있을 때는 짝이 맞기 때문에
+			stack.pop() # stack에서 pop시킴
+		else: # 여는 괄호가 없으면 짝이 맞지 않기 때문에
+			ret += 1 # +1
+ret += len(stack)
+print(ret)
