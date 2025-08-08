8582e03bea1921e9674cb6f3c9b78308903dd7e6
우강석 <oqz@naver.com>
2025-08-08 22:50:31 +0900

    [Silver IV] Title: 큐, Time: 128 ms, Memory: 111668 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+queue = []
+
+for i in range(N):
+    cmd = input().split()
+
+    if cmd[0] == "push":
+        queue.insert(0, cmd[1])
+        ##print(queue)
+
+    elif cmd[0] == "pop":
+        if len(queue) != 0: print(queue.pop())
+        else: print(-1)
+
+    elif cmd[0] == "size":
+        print(len(queue))
+
+    elif cmd[0] == "empty":
+        if len(queue) == 0: print(1)
+        else : print(0)
+
+    elif cmd[0] == "front":
+        if len(queue) == 0: print(-1)
+        else: print(queue[len(queue) -1])
+
+    elif cmd[0] == "back":
+        if len(queue) == 0: print(-1)
+        else: print(queue[0])
