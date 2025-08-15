f9b94248ea9dd2dc3eedb3be3c03617ac7bf0c43
우강석 <oqz@naver.com>
2025-08-15 15:09:43 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,18 @@
+import sys
+input = sys. stdin.readline
+
+# 입력 받기
+n, k = map(int, input().split())
+
+# 요세푸스 순열 생성
+idx = 0
+queue = [i for i in range(1, n+1)]
+answer = []
+while queue:
+    idx += k - 1  # k-1번째 인덱스까지 건너뛰기
+    if idx >= len(queue):  # 인덱스가 범위를 넘어갈 경우
+        idx %= len(queue)  # 나머지 연산을 통해 인덱스 계산
+    answer.append(str(queue.pop(idx)))  # k번째 수 제거 후 결과 배열에 추가
+
+# 결과 출력
+print("<", ", ".join(answer), ">", sep="")
