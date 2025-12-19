bf1bdc672e738d0b933ed737bf3d33c70a343ea0
우강석 <oqz@naver.com>
2025-12-19 11:33:44 +0900

    [Bronze II] Title: 줄 세우기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.py
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.py
@@ -1,12 +1,8 @@
-from collections import deque #회전큐로 구현
-
 N = int(input())
-student = list(map(int, input().split())) 
-result = deque()
+arr = list(map(int, input().split()))
+bucket = []
 
-for std, move in enumerate(student):
-    result.rotate(move) #오른쪽으로 돌림
-    result.append(std + 1) #사람 추가
-    result.rotate(-move) #왼쪽으로 원상복구
+for i in range(N):
+    bucket.insert(i-arr[i], i+1)
 
-print(*result)
+print(*bucket)
