1b56407767615fef02811e429b2e53df0b05654f
우강석 <oqz@naver.com>
2026-03-14 14:35:00 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,7 +1,12 @@
-a = int(input())                # 150
-b = int(input())                # 266
-c = int(input())                # 427
-result = list(str(a * b * c))   # [1, 7, 0, 3, 7, 3, 0, 0]
+a = int(input())
+b = int(input())
+c = int(input())
 
-for i in range(10):             # i = 0 ~ 9
-    print(result.count(str(i))) # i를 문자열(str)로 바꿔서 몇 개 있는지 count함
+result = str(a * b * c)
+
+for i in range(10):   # 0 ~ 9
+    cnt = 0
+    for j in result:  # 결과의 각 자리
+        if j == str(i):
+            cnt += 1
+    print(cnt)
